51e2990f042f72f4d177e558ad8ea87449debd04
JCastro-x <3944527630101@ingenieria.usac.edu.gt>
2026-09-16 20:00:09 -0600

    Prueba de archivo valido y Errores

--- Proyecto 1/horario_script/__init__.py
+++ Proyecto 1/horario_script/__init__.py
@@ -2,5 +2,6 @@ from .analizador_lexico import AnalizadorLexico
 from .error_lexico import ErrorLexico
 from .gestor_errores import GestorErrores
 from .token import Token
-
+#from .token import Token
+#from .token import
 __all__ = ["AnalizadorLexico", "ErrorLexico", "GestorErrores", "Token"]
